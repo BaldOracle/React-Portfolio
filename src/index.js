908e7d0de0b8a00af11871742108b76044a913ca
Mr Milk Man <sdmurphy861@gmail.com>
2023-05-13 07:42:45 -0400

    updated gitignore and npm packages.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
-//import bootstrap
-import 'bootstrap/dist/css/bootstrap.css';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+import App from './App';
+import GlobalStyles from './styles/GlobalStyle';
+import Typography from './styles/Typography';
+
+
+
+createRoot(document.getElementById('root')).render(
+  <>
+    <GlobalStyles />
+    <Typography />
+    <App />
+  </>
+);
